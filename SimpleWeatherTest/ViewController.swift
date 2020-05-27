76ab8ac489bb2b668f2c143c6a67b28b03708a2d
Thomas <thomas.legge.419@gmail.com>
2020-05-27 16:43:20 +1200

    Initial Commit

---
+++ SimpleWeatherTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleWeatherTest
+//
+//  Created by Thomas Legge on 27/05/20.
+//  Copyright © 2020 Thomas Legge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
