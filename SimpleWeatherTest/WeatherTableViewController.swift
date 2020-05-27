43ac7dad88ae9ff6e95787ab02730902a5fcbb82
Thomas <thomas.legge.419@gmail.com>
2020-05-27 23:26:29 +1200

    Added Sample Images

--- SimpleWeatherTest/WeatherTableViewController.swift
+++ SimpleWeatherTest/WeatherTableViewController.swift
@@ -17,16 +17,8 @@ class WeatherTableViewController: UITableViewController, UISearchBarDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         searchBar.delegate = self
-
         updateWeatherForLocation(location: "Auckland")
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
     @IBAction func Refresh(_ sender: UIRefreshControl) {
